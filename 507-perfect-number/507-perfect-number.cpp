e6126fcf49d03e46796b578030a6e3fad2b99509
Sunil Bharati <72141513+sunilbhara@users.noreply.github.com>
2022-06-21 03:53:15 -0700

    Time: 0 ms (100.00%), Space: 5.7 MB (99.32%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<=num/2;i++)
+        int sum=1;
+        if(num==1) return false;
+        for(int i=2;i*i<num;i++)
         {
             if(num%i==0)
             {
                 sum += i;
+                if(i!=(num/i))
+                {
+                    sum += (num/i);
+                }
             }
         }
-        
+        cout<<sum;
         if(sum==num)
         {
             return true;
