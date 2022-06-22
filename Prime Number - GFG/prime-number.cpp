864313f94e045fd783e00df559465909544a49cd
Sunil Bharati <72141513+sunilbhara@users.noreply.github.com>
2022-06-22 04:27:48 -0700

    Added solution - LeetHub

--- Prime Number - GFG/prime-number.cpp
+++ Prime Number - GFG/prime-number.cpp
@@ -9,9 +9,15 @@ public:
         // code here
         if(N==1)
             return 0;
-        for(int i=2;i*i<=N;i++)
+        if(N==2 || N==3)
+            return 1;
+        if(N%2==0 || N%3==0)
         {
-            if(N%i==0)
+            return 0;
+        }
+        for(int i=5;i*i<=N;i++)
+        {
+            if(N%i==0 || N%(i+2)==0)
                 return 0;
         }
         return 1;
