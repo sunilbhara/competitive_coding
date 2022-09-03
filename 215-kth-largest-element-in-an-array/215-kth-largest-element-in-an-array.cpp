90b4766362920c96a2665797d75e8a89e82e54c0
Sunil Bharati <72141513+sunilbhara@users.noreply.github.com>
2022-09-03 14:10:25 -0700

    Time: 142 ms (63.98%), Space: 50 MB (9.98%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        /*
+        // Solution 1: using sorting tc- O(nlogn)
         sort(nums.begin(),nums.end());
         int n = nums.size();
         return nums[n-k];
+        */
+        // Solution 2: using priority queue 
+        priority_queue<int> pq;
+        for(auto it: nums)
+            pq.push(it);
+        while(--k)
+            pq.pop();
+        return (pq.top());
     }
 };
